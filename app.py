692fd6d26ac76d7c257bb71e0cf40ac5cd9ab229
huangjiangbo <huangjiangbo.z@gmail.com>
2022-04-23 13:04:43 +0800

    add /direct_token

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ import hashlib
 import sys
 import json
 import requests
+from requests.auth import HTTPBasicAuth
+import base64
 from tools import (decode_token, generate_random_string,
                    print_json, base64_urlencode)
 from config import Config
 from client import Client
 from urllib.parse import urlencode
 from flask import redirect, request, render_template, session, abort, Flask
-
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
 
 app = Flask(__name__)
 
@@ -29,6 +32,62 @@ class UserSession:
     front_end_access_token = None
 
 
+
+
+@app.route('/likes')
+def likes():
+    print('uid: ' + request.args.get('uid'))
+    url = 'https://api.twitter.com/2/users/{0}/liked_tweets?tweet.fields=id'.format(request.args.get('uid'))
+    auth_token = request.args.get('auth_token')
+    headers = {
+        "Authorization": 'Bearer {}'.format(auth_token),
+    }
+    print(url)
+
+    r = requests.get(url, headers=headers)
+    return r.json()
+
+
+@app.route('/tlikes')
+def tlikes():
+    print('uid: ' + request.args.get('tid'))
+    url = 'https://api.twitter.com/2/tweets/{0}/liking_users'.format(request.args.get('tid'))
+    auth_token = request.args.get('auth_token')
+    headers = {
+        "Authorization": 'Bearer {}'.format(auth_token),
+    }
+    print(url)
+
+    r = requests.get(url, headers=headers)
+    return r.json()
+
+
+@app.route('/following')
+def following():
+    print('uid: ' + request.args.get('uid'))
+    url = 'https://api.twitter.com/2/users/{0}/following?user.fields=username,url'.format(request.args.get('uid'))
+    auth_token = request.args.get('auth_token')
+    headers = {
+        "Authorization": 'Bearer {}'.format(auth_token),
+    }
+    print(url)
+
+    r = requests.get(url, headers=headers)
+    return r.json()
+
+
+@app.route('/user_info')
+def user_info():
+    url = 'https://api.twitter.com/2/users/{}?&user.fields=created_at&tweet.fields=created_at'.format(request.args.get('user_id'))
+    auth_token = request.args.get('auth_token')
+    headers = {
+        "Authorization": 'Bearer {}'.format(auth_token)
+    }
+    print(url)
+    r = requests.get(url, headers=headers)
+    return r.json()
+
+
 @app.route('/who')
 def check():
     url = 'https://api.twitter.com/2/users/me'
@@ -38,6 +97,28 @@ def check():
     }
     r = requests.get(url, headers=headers)
     return r.json()
+
+
+@app.route('/get_bearer_token')
+def direct_token():
+    url = 'https://api.twitter.com/oauth2/token'
+    # basic_token = base64.b64encode("{0}:{1}".format(config['client_id'], config['client_secret']))
+    print(config['client_id'], config['client_secret'])
+
+    client_id = config['client_id']
+    client_secret = config['client_secret']
+
+    data = {
+        "grant_type": "client_credentials"
+    }
+    client = BackendApplicationClient(client_id=client_id)
+    oauth = OAuth2Session(client=client)
+    token = oauth.fetch_token(
+        token_url=url,
+        client_id=client_id,
+        client_secret=client_secret
+    )
+    return token
 
 
 @app.route('/')
@@ -82,7 +163,7 @@ def oauth_callback():
         return create_error("No code_verifier in session")
     if 'code' not in request.args:
         return create_error('No code in response')
-    
+
     token_data = callback(request.args)
     session['token_data'] = token_data
     return token_data
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 ### usage
 
-
 1. python app.py
-1. GET /
-1. GET /who   # check token
+1. GET / # start to use user's access_token
+1. GET /who # check user's info by user's access_token
 
+1. GET /direct_token # get dev access_token to get some info
